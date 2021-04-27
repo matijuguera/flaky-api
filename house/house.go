dbcffb91187fedabcb44f846e942d327ccbac7c1
Matias Juguera <mati.juguera@gmail.com>
2021-04-27 13:52:24 -0300

    add the ability to get houses

--- house/house.go
+++ house/house.go
@@ -1,5 +1,12 @@
 package house
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"time"
+)
+
 type House struct {
 	Id        int    `json:"id"`
 	Address   string `json:"address"`
@@ -11,4 +18,31 @@ type House struct {
 type HousesResponse struct {
 	Houses []House `json:"houses"`
 	Ok     bool    `json:"ok"`
+}
+
+const (
+	HOMEVISION_ENDPOINT    = "http://app-homevision-staging.herokuapp.com/api_project"
+	DEFAULT_RETRY_DURATION = 2 * time.Second
+	DEFAULT_MAX_RETRIES    = 10
+)
+
+func Get(page int) ([]House, error) {
+	//do request
+	res, err := httpretry.Get(fmt.Sprintf("%s/houses?page=%d", HOMEVISION_ENDPOINT, page), DEFAULT_RETRY_DURATION, DEFAULT_MAX_RETRIES)
+	if err != nil {
+		return []House{}, err
+	}
+	defer res.Body.Close()
+
+	//to json
+	housesResponse := HousesResponse{}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return []House{}, err
+	}
+	if err := json.Unmarshal(body, &housesResponse); err != nil {
+		return []House{}, err
+	}
+
+	return housesResponse.Houses, nil
 }
