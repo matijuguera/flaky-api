2d3c1de5cf0a69c0adb83449ce75fba7cfd2f27b
Matias Juguera <mati.juguera@gmail.com>
2021-04-27 13:54:55 -0300

    do the main behavior

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "log"
+import (
+	"log"
+	"sync"
+)
+
+const (
+	TOTAL_PAGES = 10
+)
+
+func downloadHouses(page int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	houses, err := house.Get(page)
+	if err != nil {
+		log.Printf("error getting houses page %d: %s \n", page, err)
+		return
+	}
+
+	var wgFile sync.WaitGroup
+	for _, h := range houses {
+		wgFile.Add(1)
+		go house.Download(h, &wgFile)
+	}
+	wgFile.Wait()
+}
 
 func main() {
-	log.Println("Hello flaky api")
+	var wg sync.WaitGroup
+	for i := 1; i <= TOTAL_PAGES; i++ {
+		wg.Add(1)
+		go downloadHouses(i, &wg)
+	}
+	wg.Wait()
 }
