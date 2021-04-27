b6c1808aa7f086d0cee64c581a4c2efa73f369f5
Matias Juguera <mati.juguera@gmail.com>
2021-04-27 13:46:06 -0300

    Download file added

---
+++ downloader/downloader.go
@@ -0,0 +1,35 @@
+package downloader
+
+import (
+	apierror "flaky-api/apiError"
+	"io"
+	"net/http"
+	"os"
+)
+
+func DownloadFile(URL, filePath string, fileName string) error {
+	//Get the response bytes from the url
+	resp, err := http.Get(URL)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return apierror.NewAPIError(resp.StatusCode, "downloading file error", URL, resp.Status)
+	}
+
+	//Create a empty file
+	file, err := os.Create(filePath + fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	//Write the bytes to the file
+	if _, err = io.Copy(file, resp.Body); err != nil {
+		return err
+	}
+
+	return nil
+}
