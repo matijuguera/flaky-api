e72ebe825247112f308436ae31002dec4c78ae1e
Matias Juguera <mati.juguera@gmail.com>
2021-04-27 14:56:10 -0300

    wrap errors

--- httpretry/httpretry.go
+++ httpretry/httpretry.go
@@ -3,24 +3,26 @@ package httpretry
 import (
 	"errors"
 	apierror "flaky-api/apierror"
-	"log"
+	"fmt"
 	"net/http"
 	"time"
 )
 
 func Get(URL string, retryDuration time.Duration, retries int) (*http.Response, error) {
+	errs := errors.New("max amount of retries reached, no response was found")
+
 	for i := 0; i <= retries; i++ {
 
 		resp, err := http.Get(URL)
 		if err != nil {
-			log.Println(apierror.NewAPIError(resp.StatusCode, "http retry get error", URL, resp.Status).Error())
+			errs = fmt.Errorf("%w \n -%v", errs, apierror.NewAPIError(resp.StatusCode, "http retry get error", URL, resp.Status))
 			continue
 		}
 
 		if resp != nil && resp.StatusCode == 200 {
 			return resp, nil
 		} else {
-			log.Println(apierror.NewAPIError(resp.StatusCode, "http retry status error", URL, resp.Status).Error())
+			errs = fmt.Errorf("%w \n -%v", errs, apierror.NewAPIError(resp.StatusCode, "http retry status error", URL, resp.Status))
 		}
 
 		//avoids sleep at the last iteration
@@ -29,5 +31,5 @@ func Get(URL string, retryDuration time.Duration, retries int) (*http.Response, 
 		}
 	}
 
-	return nil, errors.New("max amount of retries reached, no response was found")
+	return nil, errs
 }
